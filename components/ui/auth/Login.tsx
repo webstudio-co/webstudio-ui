c55b133580a5351352fff3cf2fec5a13e2ca4a66
Rami Bitar <rbitar@gmail.com>
2024-01-13 19:37:51 -0500

    Update

--- components/ui/auth/Login.tsx
+++ components/ui/auth/Login.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
+import { Typography } from '@mui/material'
 import { LoginForm, AuthScreen, LayoutLoader } from 'webstudio/components'
 import { useAuth } from 'webstudio/hooks'
 import { useRouter } from 'next/router'
